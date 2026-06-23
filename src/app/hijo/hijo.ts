453e6a83a8c53611effd5449e5e1d7c7e05f2316
odgm <ordyelizkh@gmail.com>
2026-06-23 15:35:52 -0300

    encapsulamiento

---
+++ src/app/hijo/hijo.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hijo',
+  imports: [],
+  templateUrl: './hijo.html',
+  styleUrl: './hijo.css',
+})
+export class Hijo {
+  private titulo = "Titulo Componente Hijo";
+
+  //Getter de TypeScript
+  get mostrarTitulo(){
+    return this.titulo;
+  }
+}
