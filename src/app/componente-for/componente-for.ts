96a158eb0ab533220e1297bb045cc1149449ce3a
odgm <ordyelizkh@gmail.com>
2026-06-30 21:52:02 -0300

    creamos el componente for

---
+++ src/app/componente-for/componente-for.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-componente-for',
+  imports: [],
+  templateUrl: './componente-for.html',
+  styleUrl: './componente-for.css',
+})
+export class ComponenteFor {}
