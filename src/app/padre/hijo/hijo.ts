7e7b391e96a3da1c294f7c6182a9abb563d16ceb
odgm <ordyelizkh@gmail.com>
2026-07-03 17:21:08 -0300

    Eliminamos el componente "hijo"

--- src/app/padre/hijo/hijo.ts
+++ src/app/padre/hijo/hijo.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hijo',
@@ -6,4 +6,6 @@ import { Component } from '@angular/core';
   templateUrl: './hijo.html',
   styleUrl: './hijo.css',
 })
-export class Hijo {}
+export class Hijo {
+  @Input() mensaje!: string;
+}
