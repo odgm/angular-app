b8e25f03b99144d852cb1921f3095fa384dd6459
odgm <ordyelizkh@gmail.com>
2026-07-05 14:32:06 -0300

    Decorador @Output

--- src/app/padre/hijo/hijo.ts
+++ src/app/padre/hijo/hijo.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-hijo',
@@ -7,6 +7,10 @@ import { Component, Input } from '@angular/core';
   styleUrl: './hijo.css',
 })
 export class Hijo {
-  // "!" operador "non-null"
-  @Input() mensaje!: string;
+  @Output() notificarPadre = new EventEmitter<string>();
+
+  enviarMensaje(){
+    //Emitir el evento con un mensaje (en este caso se emite una cadena)
+    this.notificarPadre.emit('Mensaje desde el componente hijo al padre');
+  }
 }
